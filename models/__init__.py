f4d2bf7f45e91680f3e80141d1cb05cc14126381
Guido27 <fasd@hotmail.it>
2023-09-28 11:50:39 +0200

    deletion

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from .HarDNetMSEG.HarDMSEG import HarDMSEG
 from .UACANet.UACANet import UACANet
 from .UACANet.PraNet import PraNet
 from segmentation_models_pytorch import UnetPlusPlus, Unet
+from .CE_Net.networks.cenet import CE_Net_ 
