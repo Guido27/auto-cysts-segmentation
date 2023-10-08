296b53028438e258eaecb89805b724bad2f937c8
Guido27 <fasd@hotmail.it>
2023-10-08 18:44:09 +0200

    structure_loss

--- losses/structure_loss.py
+++ losses/structure_loss.py
@@ -2,17 +2,19 @@
 import torch
 import torch.nn.functional as F
 
-
-def structure_loss(pred, mask):
+class structure_loss(nn.Module):
+    def __init__(self):
+        super(structure_loss,self).__init__()
     
-    weit = 1 + 5*torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
-    wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
-    wbce = (weit*wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
+    def forward(self, pred, mask):
+        weit = 1 + 5*torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
+        wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
+        wbce = (weit*wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
 
-    pred = torch.sigmoid(pred)
-    inter = ((pred * mask)*weit).sum(dim=(2, 3))
-    union = ((pred + mask)*weit).sum(dim=(2, 3))
-    wiou = 1 - (inter + 1)/(union - inter+1)
-    
-    return (wbce + wiou).mean()
+        pred = torch.sigmoid(pred)
+        inter = ((pred * mask)*weit).sum(dim=(2, 3))
+        union = ((pred + mask)*weit).sum(dim=(2, 3))
+        wiou = 1 - (inter + 1)/(union - inter+1)
 
+        return (wbce + wiou).mean()
+        
