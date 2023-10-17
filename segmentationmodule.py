e3d18958dcc86e0d3e13b0016c9e1e4508170412
Guido27 <fasd@hotmail.it>
2023-10-17 15:28:10 +0200

    dad

--- segmentationmodule.py
+++ segmentationmodule.py
@@ -68,7 +68,7 @@ class SegmentCyst(pl.LightningModule):
             self.hparams.optimizer,
             params=[x for x in self.model.parameters() if x.requires_grad],
         )
-        self.optimizers = [optimizer]
+        optimizer = [optimizer]
         
         if self.hparams.scheduler is not None:
             scheduler = object_from_dict(self.hparams.scheduler, optimizer=optimizer)
@@ -79,8 +79,8 @@ class SegmentCyst(pl.LightningModule):
                        'lr_scheduler': scheduler,
                        'monitor': 'val_iou'
                    }
-            return self.optimizers, [scheduler]
-        return self.optimizers
+            return optimizer, [scheduler]
+        return optimizer
     
     def log_images(self, features, masks, logits_, batch_idx):
         # logits_ is the output of the last layer of the model
@@ -118,7 +118,7 @@ class SegmentCyst(pl.LightningModule):
         # CaraNet    
         size_rates = [0.75, 1, 1.25]
         for rate in size_rates:
-            optimizer = self.optimizers(use_pl_optimizer=True)
+            optimizer = self.optimizers()
             optimizer.zero_grad()
             
             # ---- data prepare ----
