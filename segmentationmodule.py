baaf64af075952a770e65f15951fd189e3597754
Guido27 <fasd@hotmail.it>
2023-11-18 13:10:21 +0100

    done todo

--- segmentationmodule.py
+++ segmentationmodule.py
@@ -128,7 +128,6 @@ class SegmentCyst(pl.LightningModule):
 
 
     def on_train_epoch_start(self):
-        # TODO far funzionare la creazione della cartella, al momento sembra che non parta questa funzione quindi capire quando e se viene chiamata
         # create dataset folder for current epoch
         self.epoch_dataset_folder = f'epoch_datasets/epoch_{self.trainer.current_epoch}'
         Path(self.epoch_dataset_folder).mkdir(parents=True, exist_ok=True)
