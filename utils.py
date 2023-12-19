1634a7fe5e74f04f3f3b65df93771da23f952372
Guido27 <fasd@hotmail.it>
2023-12-19 11:39:08 +0100

    debug

--- utils.py
+++ utils.py
@@ -245,7 +245,7 @@ def init_training(args, hparams, name, tiling=False):
 
 def split_dataset(hparams):
     samples = get_samples(hparams["image_path"], hparams["mask_path"])
-    print(type(samples))
+    print(type(samples[0]))
     names = [file[0].stem for file in samples]
 
     df = pd.DataFrame({"filename": names,})
