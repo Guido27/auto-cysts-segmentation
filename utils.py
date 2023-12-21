252bc37ddf7b3ea9436023e73291506cd6b652e2
Guido27 <fasd@hotmail.it>
2023-12-21 19:34:04 +0100

    debug

--- utils.py
+++ utils.py
@@ -647,7 +647,7 @@ def refine_with_unfolded_patches(gt, pred, image, size=128, stride = 128):
     # pred batch of segmentation model predictions
     # batch of rgb images
     # all with dimension of 768 x 768
-    
+    print(image.shape)
     channels = image.shape[1] # RGB => 3
 
     patches = image.unfold(2,size,stride).unfold(3,size,stride).unfold(4,size,stride) 
