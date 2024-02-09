a5b0ee8be2140ed2e1dad12a8b743aedf44dc740
Guido27 <fasd@hotmail.it>
2024-02-09 14:34:00 +0100

    comment

--- segmentationmodule.py
+++ segmentationmodule.py
@@ -385,24 +385,24 @@ class SegmentCyst(pl.LightningModule):
 
             else:
                 logits = self.forward(images)
+        #
+        #for i in range(features.shape[0]):
+        #    name = batch["image_id"][i]
+        #    p = logits[i][0]
+        #    logits_ = p[i][0] #refined masks
+        #    mask = masks[i][0] #gt masks
 
-        for i in range(features.shape[0]):
-            name = batch["image_id"][i]
-            p = logits[i][0]
-            logits_ = p[i][0] #refined masks
-            mask = masks[i][0] #gt masks
-
-           
-            logits_ = (
-                logits_.cpu().numpy() > self.hparams.test_parameters["threshold"]
-            ).astype(np.uint8)
-            Image.fromarray(logits_ * 255).save(
-                self.hparams.checkpoint_callback["dirpath"]
-                / "result"
-                / "test"
-                / f"{name}.png"
-            )
-
+        #   
+        #    logits_ = (
+        #        logits_.cpu().numpy() > self.hparams.test_parameters["threshold"]
+        #    ).astype(np.uint8)
+        #    Image.fromarray(logits_ * 255).save(
+        #        self.hparams.checkpoint_callback["dirpath"]
+        #        / "result"
+        #        / "test"
+        #        / f"{name}.png"
+        #    )
+ 
         self.timing_result.loc[len(self.timing_result)] = timing
         for metric_name, metric in self.test_metrics.items():
             metric(logits, gts.int())
